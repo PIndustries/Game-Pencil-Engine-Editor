a956190c8cf85948940680f57ded1ed630d1e808
thepherm <63941518+thepherm@users.noreply.github.com>
2024-12-14 18:11:39 -0600

    Update copyright information to include P.Industries as a contributor

--- src/pawgui/pawgui_syntax_handler.cpp
+++ src/pawgui/pawgui_syntax_handler.cpp
@@ -7,6 +7,7 @@ Copyright (c) 2014-2024 Nathan Hurde, Chase Lee.
 
 Copyright (c) 2014-2024 PawByte LLC.
 Copyright (c) 2014-2024 PawByte Ambitious Working GUI(PAWGUI) contributors ( Contributors Page )
+Copyright (c) 2024 P.Industries
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the “Software”), to deal
@@ -26,9 +27,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
--PawByte Ambitious Working GUI(PAWGUI) <https://www.pawbyte.com/pawgui>
-
-
+Contributors:
+- PawByte Ambitious Working GUI(PAWGUI) <https://www.pawbyte.com/pawgui>
+- P Industries <https://p.industries>
 */
 #include "pawgui_syntax_handler.h"
 #include "pawgui_themes.h"
